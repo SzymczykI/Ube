a5242556e5bd4b6fda2f49910aadf282662ed589
Izabela Szymczyk <izabela.szymczyk@vp.pl>
2023-02-07 19:07:21 +0100

    navigation card improvement

--- components/NavigateCard.tsx
+++ components/NavigateCard.tsx
@@ -1,4 +1,10 @@
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
 import tw from "twrnc";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
@@ -7,6 +13,7 @@ import { useDispatch } from "react-redux";
 import { setDestination } from "../slices/navSlice";
 import { useNavigation } from "@react-navigation/native";
 import NavFavourites from "./NavFavourites";
+import { Icon } from "react-native-elements";
 
 const NavigateCard = () => {
   const dispatch = useDispatch();
@@ -14,7 +21,7 @@ const NavigateCard = () => {
 
   return (
     <SafeAreaView style={tw`bg-white flex-1`}>
-      <Text style={tw`text-center py-5 text-xl`}>Hello!</Text>
+      <Text style={tw`text-center py-2`}>Hello!</Text>
       <View style={tw`border-t border-gray-200 flex-shrink`}>
         <View>
           <GooglePlacesAutocomplete
@@ -38,12 +45,33 @@ const NavigateCard = () => {
                 );
               }
 
-              navigation.navigate('RideOptionsCard')
+              navigation.navigate("RideOptionsCard");
             }}
           />
         </View>
 
         <NavFavourites />
+      </View>
+
+      <View style={tw`flex-row bg-white justify-evenly border-t border-gray-100`}>
+        <TouchableOpacity
+          style={tw`flex flex-row bg-black justify-between w-24 px-4 py-3 rounded-full`}
+        onPress={() => navigation.navigate("RideOptionsCard")}
+        >
+          <Icon name="car" type="font-awesome" color="white" size={16} />
+          <Text style={tw`text-white text-center`}>Rides</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={tw`flex flex-row justify-between w-24 px-4 py-3 rounded-full`}
+        >
+          <Icon
+            name="fast-food-outline"
+            type="ionicon"
+            color="black"
+            size={16}
+          />
+          <Text style={tw` text-center`}>Eats</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
@@ -54,7 +82,7 @@ export default NavigateCard;
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "white",
-    paddingTop: 20,
+    paddingTop: 5,
     flex: 0,
   },
   textInput: {
